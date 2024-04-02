c0967f9e7e79984d03e62dfb955c38b88999992a
sangngo552004 <sangngo552004@gmail.com>
2024-04-02 16:49:08 +0700

    connect database && create model tour

--- index.ts
+++ index.ts
@@ -2,8 +2,7 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
-import sequelize from "./config/database";
-sequelize;
+import Tour from "./models/tour.model";
 
 const app : Express = express();
 const port : (number | string) = process.env.PORT || 3000;
@@ -11,8 +10,19 @@ const port : (number | string) = process.env.PORT || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/tours", (req : Request, res : Response) => {
-    res.render("client/pages/tours/index.pug")
+app.get("/tours",async  (req : Request, res : Response) => {
+    //SELECT * FROM tours WHERE deleted = false AND status = "active";
+    const tours = await Tour.findAll({
+        where: {
+            deleted : false,
+            status: "active"
+        },
+        raw : true
+    });
+    res.render("client/pages/tours/index.pug", {
+        pageTitle : "Danh sach tour",
+        tours : tours
+    })
 });
 
 app.listen(port, () => {
